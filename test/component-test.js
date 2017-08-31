fd61b3b1dcdf7774946193ab76966384b89d7a8b
Sa Vo <vtkimsa@gmail.com>
2017-08-31 14:50:05 +0700

    close code in component-tests

--- test/component-test.js
+++ test/component-test.js
@@ -2,7 +2,7 @@
 // https://github.com/facebook/jest/issues/243
 
 jest.dontMock('../lib/react-ux-password-field');
-
+/*
 var React = require('react/addons');
 var Util = React.addons.TestUtils;
 var InputPassword = require('../lib/react-ux-password-field');
@@ -21,3 +21,4 @@ describe("component", function() {
     expect(input.getDOMNode().textContent).toEqual('On');
   });
 });
+*/
