588a62c56b0e8bc168035f174c828f3ac1f8679a
Sa Vo <vtkimsa@gmail.com>
2017-09-01 14:09:23 +0700

    update code

--- app.js
+++ app.js
@@ -3,4 +3,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Accordion = require('components/accordionComponent');
-ReactDOM.render(<Accordion/>, document.getElementById('view'));
+var input = [{
+  name: 'Title 1',
+  content: 'Content belongs to title 1'
+},{
+  name: 'Title 2',
+  content: 'Content belongs to title 2'
+}];
+
+ReactDOM.render(<Accordion data={input}/>, document.getElementById('view'));
