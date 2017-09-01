1e35271f4ebd69c511685645f1b9d6bb14070fa8
Sa Vo <vtkimsa@gmail.com>
2017-09-01 12:16:10 +0700

    remove jspm

---
+++ __tests__/accordionComponent-spec.js
@@ -0,0 +1,19 @@
+jest.dontMock('../scripts/accordionComponent.js');
+
+describe('Accordion', function() {
+  var React = require('react');
+  var ReactDOM = require('react-dom');
+  var TestUtils = require('react-addons-test-utils');
+
+  var Accordion;
+
+  beforeEach(function() {
+    Accordion = require('../scripts/accordionComponent');
+  });
+
+  it('should exists', function() {
+    // Render into document
+    var accordion = TestUtils.renderIntoDocument( <Accordion /> );
+    expect(TestUtils.isCompositeComponent(accordion)).toBeTruthy();
+  });
+});
